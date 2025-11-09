ece952f678e7c2c01fd68c04e96746ae07465ab9
Eliana Ḿendez <eliamargi@gmail.com>
2025-11-09 12:39:40 -0500

    updates

---
+++ src/step-definitions/s0_home_page.steps.js
@@ -0,0 +1,8 @@
+import { Given, When, Then } from '@wdio/cucumber-framework';
+import { browser, expect } from '@wdio/globals';
+import p0_homePage from '../pageObjects/p0_home.page';
+
+When('the user clicks on a product title or image', async () => {
+    
+
+}); 
