0a336deafa39cd191ef393b2f47832e243d33d07
Eliana Ḿendez <eliamargi@gmail.com>
2026-04-16 11:18:15 -0500

    CI configuration upadate

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -46,8 +46,7 @@ export const config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: process.env.CI ? 1 : (process.env.WDIO_MAX_INSTANCES ? Number(process.env.WDIO_MAX_INSTANCES) : 3),
-    //
+    maxInstances: process.env.CI === 'true' ? 1 : 10,
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
